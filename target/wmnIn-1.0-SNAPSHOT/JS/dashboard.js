bdacf875f0fcfdbbad0e129e6b677f8ef198f313
michellesnz <81836924+michellesnz@users.noreply.github.com>
2023-05-13 22:36:34 -0500

    :)

--- target/wmnIn-1.0-SNAPSHOT/JS/dashboard.js
+++ target/wmnIn-1.0-SNAPSHOT/JS/dashboard.js
@@ -22,19 +22,18 @@ function getRecentPosts(){
             
             for(var i=0; i<Object.keys(data).length; i++){
                 console.log("POST ", data[i]);
-                $("#posts").append(
-                    $("<div>").addClass("card contentItem").append(
-                        $("<div>").addClass("card-header").append(
-                            $("<img>").attr("src", data[i].media).addClass("img-fluid rounded-circle pfpNewpost")
-                            .append(data[i].postUserdata)).append(
-                        $("<div>").addClass("card-header").append(
-                            $("<h5>").text(data[i].title)
-                            .append($("<p>").text(data[i].description))).append(
-                        $("<div>").addClass("card-footer").append(
-                            $("<p>").append("<i>").addClass("icon ion-md-heart pe-2").text("0"))
-                        ))
-                    )
-                );    
+               $("#posts")
+                .append( $("<div>").addClass("card contentItem")
+                    .append($("<div>").addClass("card-header")
+                        .append($("<img>").attr("src", data[i].media).addClass("img-fluid rounded-circle pfpNewpost")
+                        .text(data[i].postUserdata)))
+                    .append($("<div>").addClass("card-body")
+                        .append($("<h5>")).text(data[i].title)
+                        .append($("<p>")).text(data[i].description)
+                        .append($("<img>")).attr("src", data[i].media).addClass("img-fluid postImg"))
+                    .append($("<div>").addClass("card-footer")
+                        .append($("<p>")
+                            .append($("<i>")).addClass("icon ion-md-heart pe-2")).text("100"))); 
             }
         },
         error: function(xhr, data, error) {
